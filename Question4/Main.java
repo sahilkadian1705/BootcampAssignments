56e61cb2999598b3414cec20c22b034a5bbfd19a
sahilkadian1705 <sahil.kadian@tothenew.com>
2023-03-15 09:48:28 +0530

    HashCode and Equals Method added

--- Question4/Main.java
+++ Question4/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Employee e1=new Employee("Sahil",21,"Trainee");
         Employee e2=new Employee("Harsh",21,"QE");
-        Employee e3=new Employee("Anoop",23,"Devops");
+        Employee e3=new Employee("Shubham",23,"Devops");
         Employee e4=new Employee("Rajat",22,"Sde");
 
         Map<Employee,Integer> map=new HashMap<>();
@@ -13,8 +13,6 @@ public class Main {
         map.put(e2,250000);
         map.put(e3,75000);
         map.put(e4,100000);
-
-        System.out.println(e1.toString());
-
+        System.out.println(e1.equals(e3));
     }
 }
