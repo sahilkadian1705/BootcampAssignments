9269a6116f2ef0d13e4ce4494b240b2fc157cd91
sahilkadian1705 <sahil.kadian@tothenew.com>
2023-04-03 12:51:06 +0530

    Controller Added

--- src/main/java/com/sahilkadian/SpringBoot/Application.java
+++ src/main/java/com/sahilkadian/SpringBoot/Application.java
@@ -1,15 +1,11 @@
 package com.sahilkadian.SpringBoot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
-	@Autowired
-	EmployeeDao employeeDao;
 
 	public static void main(String[] args) {
 		ApplicationContext ac=SpringApplication.run(Application.class, args);
--- src/main/java/com/sahilkadian/SpringBoot/EmployeeController.java
+++ src/main/java/com/sahilkadian/SpringBoot/EmployeeController.java
@@ -1,2 +1,32 @@
-package com.sahilkadian.SpringBoot;public class EmployeeController {
+package com.sahilkadian.SpringBoot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class EmployeeController implements CommandLineRunner {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    List<Employee> employeeList = new ArrayList<>();
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployeeList(){
+        employeeDao.findAll().forEach(employeeList::add);
+        return employeeList;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        employeeDao.save(new Employee(1L,"Sahil","SDE"));
+        employeeDao.save(new Employee(2L,"Nitin","SDE"));
+        employeeDao.save(new Employee(3L,"Shubham","SDE"));
+        employeeDao.save(new Employee(4L,"Rajat","SDE"));
+    }
 }
