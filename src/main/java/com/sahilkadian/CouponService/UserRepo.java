00954294a5f9931a56c4473847ba2a0962081302
sahilkadian1705 <sahil.kadian@tothenew.com>
2023-04-13 17:21:37 +0530

    OAuth Coupon Service

---
+++ src/main/java/com/sahilkadian/CouponService/UserRepo.java
@@ -0,0 +1,7 @@
+package com.sahilkadian.CouponService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
